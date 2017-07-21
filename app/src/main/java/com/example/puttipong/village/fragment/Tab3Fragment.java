d83d207385bf35310f83d4143e123dec48ba6f16
zern3w <zer.n3w@gmail.com>
2017-07-22 01:34:56 +0700

    update village's list and maps

--- app/src/main/java/com/example/puttipong/village/fragment/Tab3Fragment.java
+++ app/src/main/java/com/example/puttipong/village/fragment/Tab3Fragment.java
@@ -1,12 +1,16 @@
-package com.example.puttipong.village;
+package com.example.puttipong.village.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class Tab3Fragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Tab3Fragment extends SupportMapFragment implements OnMapReadyCallback{
+    private GoogleMap mMap;
 
     public Tab3Fragment() {
         super();
@@ -17,18 +21,6 @@ public class Tab3Fragment extends Fragment {
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_tab3, container, false);
-        initInstances(rootView);
-        return rootView;
-    }
-
-    private void initInstances(View rootView) {
-        // Init 'View' instance(s) with rootView.findViewById here
     }
 
     @Override
@@ -56,8 +48,22 @@ public class Tab3Fragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        getMapAsync(this);
+
         if (savedInstanceState != null) {
             // Restore Instance State here
         }
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 }
